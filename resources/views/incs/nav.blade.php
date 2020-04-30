998bc7e28ea0fddf800655331d11d030b3aeed7e
egar-pro <ethangardner27@gmail.com>
2020-04-30 00:11:02 -0500

    initial commit

---
+++ resources/views/incs/nav.blade.php
@@ -0,0 +1,4 @@
+<nav class="navbar navbar-light bg-light">
+    <a class="navbar-brand" href="/customers">Show Customers</a>
+    <a class="navbar-brand" href="/customers/create">Add Customer</a>
+  </nav>
